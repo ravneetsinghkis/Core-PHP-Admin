1e43310538be786f626116d9932886821b630c6a
ravneetsinghkis <85344472+ravneetsinghkis@users.noreply.github.com>
2024-05-03 17:43:35 +0530

    Core-PHP-Admin

---
+++ admin/send-email.php
@@ -0,0 +1,24 @@
+<?php
+$to = $_POST['email_id'];
+$subject = 'Invite Email';
+$headers =  'MIME-Version: 1.0' . "\r\n"; 
+$headers .= 'From: Your name <info@address.com>' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 
+$message = 'Hi '.$to;
+$message .= 'This is a Invite email sent from Admin. Please click on this <a href="http://localhost/core-php/signup.php?inviteuser='.base64_encode($to).'">link</a> and complete your registration process.';
+// send email
+$mail_sent=mail($to,$subject,$message,$headers);
+$response = array();
+    if($mail_sent){
+        $response = array(
+            'status' => true,
+            'message' => 'Mail sent succesfully',
+        );
+    }else{
+         $response = array(
+            'status' => false,
+            'message' => 'Something Went Wrong. Please try again later',
+        );
+    }
+echo json_encode($response);
+?>
